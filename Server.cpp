863220225b6bbc00eb7cd981f7cabae8ca4f70fd
Lukas <lukasurin@gmail.com>
2013-03-14 20:59:59 +0100

    Messaging
    -Added - Spracovanie sprav, lepsie spracovavanie uzivatelov. Mozne
    napojenie len dvoch ludi. Natvrdo zadane pozicie v premennej connections
    ( na prvom mieste biely hrac na druhom cierny). Spravne ukoncovanie
    pripojeni a pridane spravne zistenie ci spojenie bolo ukoncene klientom.

--- Server.cpp
+++ Server.cpp
@@ -1,9 +1,12 @@
 #include "Server.h"
+#include <vector>
 
 using namespace std;
 
 SOCKET * Connections;
 int counter;
+
+int whiteSide = -1;
 
 struct Buffer {
 	int ID;
@@ -19,9 +22,16 @@ int ServerThread(int ID) {
 	char * sendmsg = new char[sizeof(Buffer)];
 	ZeroMemory(sendmsg, sizeof(Buffer));
 
-	for (;; Sleep(10)) {
+	int nRet;
 
-		if (recv(Connections[ID], rcvmsg, 256, NULL)) {
+	for (;; Sleep(10)) {
+		if ((nRet = recv(Connections[ID], rcvmsg, 256, NULL)) == SOCKET_ERROR) {
+			cout << "Player Disconnected " << ID << endl;			
+			if (whiteSide == ID) {
+				whiteSide = -1;							
+			}
+			break;
+		} else {
 			sBuffer.ID = ID;
 			memcpy(sBuffer.message, rcvmsg, 256);
 			memcpy(sendmsg, &sBuffer, sizeof(Buffer));
@@ -31,13 +41,15 @@ int ServerThread(int ID) {
 			} else {
 				send(Connections[i], sendmsg, sizeof(Buffer), NULL);
 			}
-
+		
 	}
+	closesocket(Connections[ID]);
+	counter--;
 	return 0;
 }
 
 void main() {
-	char message[200];
+	char message[256];
 	string strmessage;
 
 	long answer;
@@ -78,16 +90,37 @@ void main() {
 
 		cout << "Waiting for connection" << endl;
 
-		if (sConnect = accept(sListen, (SOCKADDR *)&addr, &adrlen)) {
+		if (sConnect = accept(sListen, (SOCKADDR *)&addr, &adrlen)) {			
 			cout << "Connection found " << endl;	
+					
+			Buffer sBuffer;			
+			string toSend = "WELCOME ";
+			if (whiteSide == -1) {
+				Connections[0] = sConnect;
+				sBuffer.ID = 0;
+				toSend.append("1");
+				whiteSide = counter;
 
-			Connections[counter] = sConnect;
+				ZeroMemory(sBuffer.message, 256);
+				memcpy(sBuffer.message, toSend.c_str(), toSend.size());
 
-			char * ID = new char[64];
-			ZeroMemory(ID, sizeof(ID));
+				char * sendMsg = new char[sizeof(Buffer)];
+				memcpy(sendMsg, &sBuffer, sizeof(Buffer));
 
-			itoa(counter, ID, 10);
-			send(Connections[counter], ID, sizeof(ID), NULL);
+				send(Connections[counter], sendMsg, sizeof(Buffer), NULL);
+			} else {
+				Connections[1] = sConnect;
+				sBuffer.ID = 1;
+				toSend.append("0");
+
+				ZeroMemory(sBuffer.message, 256);
+				memcpy(sBuffer.message, toSend.c_str(), toSend.size());
+
+				char * sendMsg = new char[sizeof(Buffer)];
+				memcpy(sendMsg, &sBuffer, sizeof(Buffer));
+
+				send(Connections[counter], sendMsg, sizeof(Buffer), NULL);
+			}												
 
 			counter++;
 			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE) ServerThread,(LPVOID)(counter - 1), NULL, NULL);
